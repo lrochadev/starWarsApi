c6ee123582875723809d7aaa75b92572ab7dabdf
Leonardo Rocha <lrochadev@gmail.com>
2020-07-13 20:20:22 -0300

    Add repository annotation

--- src/main/java/br/com/desafio/b2w/starWarsApi/repository/PlanetaRepository.java
+++ src/main/java/br/com/desafio/b2w/starWarsApi/repository/PlanetaRepository.java
@@ -1,17 +1,16 @@
 package br.com.desafio.b2w.starWarsApi.repository;
 
+import br.com.desafio.b2w.starWarsApi.model.Planeta;
+import org.springframework.data.mongodb.repository.MongoRepository;
+import org.springframework.stereotype.Repository;
+
 import java.util.List;
 import java.util.Optional;
 
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-import br.com.desafio.b2w.starWarsApi.model.Planeta;
-
 /**
- * 
  * @author Leonardo Rocha
- *
  */
-public interface PlanetaRepository extends MongoRepository<Planeta, String>{
-	Optional<List<Planeta>> findByNomeIgnoreCaseContaining(String nome);
+@Repository
+public interface PlanetaRepository extends MongoRepository<Planeta, String> {
+    Optional<List<Planeta>> findByNomeIgnoreCaseContaining(String nome);
 }
