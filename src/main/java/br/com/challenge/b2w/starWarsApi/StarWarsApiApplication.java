6ee3add7cd52bae42224cf3d64c85b55e19f0a38
Leonardo Rocha <lrochadev@gmail.com>
2024-04-23 16:46:21 -0300

    feat: upgrade spring boot parent from 2.2.6 to 2.7.18.

--- src/main/java/br/com/challenge/b2w/starWarsApi/StarWarsApiApplication.java
+++ src/main/java/br/com/challenge/b2w/starWarsApi/StarWarsApiApplication.java
@@ -1,15 +1,17 @@
 package br.com.challenge.b2w.starWarsApi;
 
-import br.com.challenge.b2w.starWarsApi.swagger.RetryHandlerConfiguration;
+import com.fasterxml.jackson.annotation.JsonAutoDetect;
 import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.fasterxml.jackson.databind.util.StdDateFormat;
+import com.fasterxml.jackson.annotation.PropertyAccessor;
+import com.fasterxml.jackson.databind.DeserializationFeature;
+import com.fasterxml.jackson.databind.MapperFeature;
+import com.fasterxml.jackson.databind.SerializationFeature;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.databind.module.SimpleModule;
 import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
 import org.apache.http.client.config.RequestConfig;
 import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.HttpClientBuilder;
-import org.apache.http.impl.conn.PoolingHttpClientConnectionManager;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.MessageSource;
@@ -20,10 +22,14 @@ import org.springframework.data.mongodb.repository.config.EnableMongoRepositorie
 import org.springframework.http.client.BufferingClientHttpRequestFactory;
 import org.springframework.http.client.ClientHttpRequestFactory;
 import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.json.Jackson2ObjectMapperBuilder;
+import org.springframework.http.converter.HttpMessageConverter;
+import org.springframework.http.converter.StringHttpMessageConverter;
 import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
 import org.springframework.web.client.RestTemplate;
 
+import java.nio.charset.StandardCharsets;
+import java.util.LinkedList;
+import java.util.List;
 import java.util.Properties;
 
 /**
@@ -33,30 +39,6 @@ import java.util.Properties;
 @EnableMongoRepositories(value = "br.com.challenge.b2w.starWarsApi.repository", considerNestedRepositories = true)
 @EnableMongoAuditing
 public class StarWarsApiApplication {
-
-    @Value("${restTemplate.pool.connectionRequestTimeout}")
-    private int connectionRequestTimeout;
-
-    @Value("${restTemplate.pool.connectionTimeout}")
-    private int connectTimeout;
-
-    @Value("${restTemplate.pool.socketTimeout}")
-    private int socketTimeout;
-
-    @Value("${restTemplate.pool.maxConnections}")
-    private int maxConnections;
-
-    @Value("${restTemplate.pool.maxPerRoute}")
-    private int maxPerRoute;
-
-    @Value("${restTemplate.pool.validateAfterInactivity}")
-    private int validateAfterInactivity;
-
-    @Value("${restTemplate.pool.retryCount}")
-    private int retryCount;
-
-    @Value("${restTemplate.pool.retrySleepTimeMS}")
-    private int retrySleepTimeMS;
 
     public static void main(String[] args) {
         SpringApplication.run(StarWarsApiApplication.class, args);
@@ -75,35 +57,55 @@ public class StarWarsApiApplication {
     }
 
     @Bean
-    public ObjectMapper objectMapper() {
-        final Jackson2ObjectMapperBuilder builder = new Jackson2ObjectMapperBuilder();
-        builder.indentOutput(true).dateFormat(StdDateFormat.getDateInstance()).failOnEmptyBeans(true).failOnUnknownProperties(true);
-        return builder.build().registerModule(new JavaTimeModule()).setSerializationInclusion(JsonInclude.Include.NON_NULL);
+    public JsonMapper jsonMapper() {
+
+        return JsonMapper.builder()
+                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
+                .disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
+                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
+                .disable(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE)
+                .disable(SerializationFeature.INDENT_OUTPUT)
+                .serializationInclusion(JsonInclude.Include.NON_NULL)
+                .visibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
+                .visibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
+                .visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
+                .addModule(new JavaTimeModule())
+                .addModule(new SimpleModule())
+                .build();
     }
 
     @Bean
-    public RestTemplate restTemplate(MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter) {
+    public RestTemplate restTemplate() {
         final RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
-        restTemplate.getMessageConverters().add(0, mappingJackson2HttpMessageConverter);
+        restTemplate.setMessageConverters(messageConverters());
         return restTemplate;
+    }
+
+    @Bean
+    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
+        return new MappingJackson2HttpMessageConverter(jsonMapper());
+    }
+
+    private List<HttpMessageConverter<?>> messageConverters() {
+        final List<HttpMessageConverter<?>> messageConverters = new LinkedList<>();
+        messageConverters.add(mappingJackson2HttpMessageConverter());
+        messageConverters.add(stringHttpMessageConverter());
+        return messageConverters;
+    }
+
+    private StringHttpMessageConverter stringHttpMessageConverter() {
+        return new StringHttpMessageConverter(StandardCharsets.UTF_8);
     }
 
     private ClientHttpRequestFactory clientHttpRequestFactory() {
         final HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
 
         final RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectionRequestTimeout(connectionRequestTimeout)
-                .setConnectTimeout(connectTimeout)
-                .setSocketTimeout(socketTimeout).build();
-
-        final PoolingHttpClientConnectionManager poolingHttpClientConnectionManager = new PoolingHttpClientConnectionManager();
-        poolingHttpClientConnectionManager.setMaxTotal(maxConnections);
-        poolingHttpClientConnectionManager.setDefaultMaxPerRoute(maxPerRoute);
-        poolingHttpClientConnectionManager.setValidateAfterInactivity(validateAfterInactivity);
+                .setConnectionRequestTimeout(1000)
+                .setConnectTimeout(5000)
+                .setSocketTimeout(1000).build();
 
         final CloseableHttpClient httpClientBuilder = HttpClientBuilder.create()
-                .setConnectionManager(poolingHttpClientConnectionManager)
-                .setRetryHandler(new RetryHandlerConfiguration(retryCount, retrySleepTimeMS))
                 .setDefaultRequestConfig(requestConfig)
                 .build();
 
