8fca0f52d5c19737207fb2488dac14950c4d2b1a
Leonardo Rocha <lrochadev@gmail.com>
2024-04-23 17:57:05 -0300

    feat: removing response of @ApiOperation.

--- src/main/java/br/com/challenge/b2w/starWarsApi/resources/PlanetResource.java
+++ src/main/java/br/com/challenge/b2w/starWarsApi/resources/PlanetResource.java
@@ -10,9 +10,12 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.Collection;
+import java.util.Collections;
 import java.util.List;
 import java.util.Optional;
 
+import static java.util.Collections.emptyList;
 import static org.springframework.http.HttpStatus.OK;
 
 /**
@@ -27,33 +30,33 @@ public class PlanetResource {
     private final PlanetService planetService;
 
     @GetMapping
-    @ApiOperation(value = "${message.info.swagger.endpoint.planet.list}", response = Planet[].class)
+    @ApiOperation(value = "${message.info.swagger.endpoint.planet.list}")
     public ResponseEntity<List<Planet>> list() {
         return new ResponseEntity<>(this.planetService.findAll(), OK);
     }
 
     @PostMapping
-    @ApiOperation(value = "${message.info.swagger.endpoint.planet.create}", response = Planet[].class)
+    @ApiOperation(value = "${message.info.swagger.endpoint.planet.create}")
     public ResponseEntity<Planet> create(@Validated @RequestBody Planet planet) {
         return ResponseEntity.status(HttpStatus.CREATED).body(this.planetService.save(planet));
     }
 
     @DeleteMapping("/{id}")
     @ResponseStatus(code = HttpStatus.NO_CONTENT)
-    @ApiOperation(value = "${message.info.swagger.endpoint.planet.delete}", response = Planet[].class)
+    @ApiOperation(value = "${message.info.swagger.endpoint.planet.delete}")
     public void delete(@PathVariable String id) {
         this.planetService.delete(id);
     }
 
     @GetMapping("/{id}")
-    @ApiOperation(value = "${message.info.swagger.endpoint.planet.find.byid}", response = Planet[].class)
+    @ApiOperation(value = "${message.info.swagger.endpoint.planet.find.byid}")
     public ResponseEntity<Planet> findById(@PathVariable String id) {
         return new ResponseEntity<>(planetService.findById(id), OK);
     }
 
     @GetMapping("/name/{name}")
-    @ApiOperation(value = "${message.info.swagger.endpoint.planet.find.byname}", response = Planet[].class)
-    public ResponseEntity<Optional<List<Planet>>> findByNome(@PathVariable String name) {
-        return new ResponseEntity<>(planetService.findByName(name), OK);
+    @ApiOperation(value = "${message.info.swagger.endpoint.planet.find.byname}")
+    public ResponseEntity<List<Planet>> findByNome(@PathVariable String name) {
+        return new ResponseEntity<>(planetService.findByName(name).orElse(emptyList()), OK);
     }
 }
