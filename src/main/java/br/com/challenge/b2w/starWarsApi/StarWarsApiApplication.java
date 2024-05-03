02eea9f3837fca5984436f3768bbcf50cbb66c29
Leonardo Rocha <lrochadev@gmail.com>
2024-05-03 18:28:52 -0300

    feat: moving application config to a new class.

--- src/main/java/br/com/challenge/b2w/starWarsApi/StarWarsApiApplication.java
+++ src/main/java/br/com/challenge/b2w/starWarsApi/StarWarsApiApplication.java
@@ -1,38 +1,9 @@
 package br.com.challenge.b2w.starWarsApi;
 
-import com.fasterxml.jackson.annotation.JsonAutoDetect;
-import com.fasterxml.jackson.annotation.JsonInclude;
-import com.fasterxml.jackson.annotation.PropertyAccessor;
-import com.fasterxml.jackson.databind.DeserializationFeature;
-import com.fasterxml.jackson.databind.MapperFeature;
-import com.fasterxml.jackson.databind.SerializationFeature;
-import com.fasterxml.jackson.databind.json.JsonMapper;
-import com.fasterxml.jackson.databind.module.SimpleModule;
-import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
-import org.apache.hc.client5.http.config.RequestConfig;
-import org.apache.hc.client5.http.impl.classic.CloseableHttpClient;
-import org.apache.hc.client5.http.impl.classic.HttpClientBuilder;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.MessageSource;
-import org.springframework.context.annotation.Bean;
-import org.springframework.context.support.ReloadableResourceBundleMessageSource;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
-import org.springframework.http.client.BufferingClientHttpRequestFactory;
-import org.springframework.http.client.ClientHttpRequestFactory;
-import org.springframework.http.client.HttpComponentsClientHttpRequestFactory;
-import org.springframework.http.converter.HttpMessageConverter;
-import org.springframework.http.converter.StringHttpMessageConverter;
-import org.springframework.http.converter.json.MappingJackson2HttpMessageConverter;
-import org.springframework.web.client.RestTemplate;
-
-import java.nio.charset.StandardCharsets;
-import java.util.LinkedList;
-import java.util.List;
-import java.util.Properties;
-
-import static org.apache.hc.core5.util.Timeout.ofSeconds;
 
 /**
  * @author Leonardo Rocha
@@ -45,76 +16,4 @@ public class StarWarsApiApplication {
     public static void main(String[] args) {
         SpringApplication.run(StarWarsApiApplication.class, args);
     }
-
-    @Bean
-    public MessageSource messageSource() {
-        final ReloadableResourceBundleMessageSource messageSource = new ReloadableResourceBundleMessageSource();
-        final Properties properties = new Properties();
-        properties.setProperty("fileEncodings", "UTF-8");
-        messageSource.setBasename("classpath:messages");
-        messageSource.setDefaultEncoding("UTF-8");
-        messageSource.setFileEncodings(properties);
-
-        return messageSource;
-    }
-
-    @Bean
-    public JsonMapper jsonMapper() {
-
-        return JsonMapper.builder()
-                .configure(MapperFeature.ACCEPT_CASE_INSENSITIVE_PROPERTIES, true)
-                .disable(DeserializationFeature.FAIL_ON_IGNORED_PROPERTIES)
-                .disable(DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES)
-                .disable(DeserializationFeature.FAIL_ON_INVALID_SUBTYPE)
-                .disable(SerializationFeature.INDENT_OUTPUT)
-                .serializationInclusion(JsonInclude.Include.NON_NULL)
-                .visibility(PropertyAccessor.GETTER, JsonAutoDetect.Visibility.NONE)
-                .visibility(PropertyAccessor.SETTER, JsonAutoDetect.Visibility.NONE)
-                .visibility(PropertyAccessor.FIELD, JsonAutoDetect.Visibility.ANY)
-                .addModule(new JavaTimeModule())
-                .addModule(new SimpleModule())
-                .build();
-    }
-
-    @Bean
-    public RestTemplate restTemplate() {
-        final RestTemplate restTemplate = new RestTemplate(clientHttpRequestFactory());
-        restTemplate.setMessageConverters(messageConverters());
-        return restTemplate;
-    }
-
-    @Bean
-    public MappingJackson2HttpMessageConverter mappingJackson2HttpMessageConverter() {
-        return new MappingJackson2HttpMessageConverter(jsonMapper());
-    }
-
-    private List<HttpMessageConverter<?>> messageConverters() {
-        final List<HttpMessageConverter<?>> messageConverters = new LinkedList<>();
-        messageConverters.add(mappingJackson2HttpMessageConverter());
-        messageConverters.add(stringHttpMessageConverter());
-        return messageConverters;
-    }
-
-    private StringHttpMessageConverter stringHttpMessageConverter() {
-        return new StringHttpMessageConverter(StandardCharsets.UTF_8);
-    }
-
-    private ClientHttpRequestFactory clientHttpRequestFactory() {
-        final HttpComponentsClientHttpRequestFactory requestFactory = new HttpComponentsClientHttpRequestFactory();
-
-        final RequestConfig requestConfig = RequestConfig.custom()
-                .setConnectionRequestTimeout(ofSeconds(1000))
-                .setConnectionRequestTimeout(ofSeconds(5000))
-                .setResponseTimeout(ofSeconds(1000))
-                .build();
-
-        final CloseableHttpClient httpClientBuilder = HttpClientBuilder.create()
-                .setDefaultRequestConfig(requestConfig)
-                .build();
-
-        requestFactory.setHttpClient(httpClientBuilder);
-
-        return new BufferingClientHttpRequestFactory(requestFactory);
-    }
-
 }
