30d3a067f3b248020941d0e0a462db34963a474b
Leonardo Rocha <lrochadev@gmail.com>
2026-03-24 19:17:27 -0300

    refactor(swapi): migrate SwapiService to use RestClient and Slf4j
    Replaces the deprecated `RestTemplate` with Spring's modern `RestClient` for external API calls within the `SwapiService`. This aligns the service with current Spring Boot 3/4+ recommendations and leverages the `RestClient` configuration. Additionally, logging is streamlined by adopting Lombok's `@Slf4j` annotation.

--- src/main/java/br/com/challenge/b2w/starWarsApi/services/SwapiService.java
+++ src/main/java/br/com/challenge/b2w/starWarsApi/services/SwapiService.java
@@ -5,13 +5,11 @@ import br.com.challenge.b2w.starWarsApi.dto.swapi.SwapiDto;
 import br.com.challenge.b2w.starWarsApi.exception.SWAPIException;
 import br.com.challenge.b2w.starWarsApi.utils.MessageUtil;
 import lombok.RequiredArgsConstructor;
-import org.apache.logging.log4j.LogManager;
-import org.apache.logging.log4j.Logger;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Value;
-import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Service;
+import org.springframework.web.client.RestClient;
 import org.springframework.web.client.RestClientException;
-import org.springframework.web.client.RestTemplate;
 
 import java.util.ArrayList;
 import java.util.List;
@@ -21,38 +19,34 @@ import static java.util.Optional.ofNullable;
 /**
  * @author Leonardo Rocha
  */
+@Slf4j
 @Service
 @RequiredArgsConstructor
 public class SwapiService {
-
-    private static final Logger logger = LogManager.getLogger(SwapiService.class);
 
     @Value("${starwars.api.url}")
     private String starWarsApiUrl;
 
     private final MessageUtil message;
-
-    private final RestTemplate restTemplate;
+    private final RestClient restClient;
 
     public SwapiDto consultSwAPI(final String planetName) {
 
-        logger.info("Finding planet : {}, in SWAPI.", planetName);
+        log.info("Finding planet : {}, in SWAPI.", planetName);
 
         try {
+            final SwapiDto response = restClient.get()
+                    .uri(starWarsApiUrl + planetName)
+                    .retrieve()
+                    .body(SwapiDto.class);
 
-            final ResponseEntity<SwapiDto> response = restTemplate.getForEntity(this.starWarsApiUrl + planetName, SwapiDto.class);
-
-            if (response.getStatusCode().is2xxSuccessful()) {
-                logger.info("Found: {} planet!", planetName);
-                return response.getBody();
-            }
+            log.info("Found: {} planet!", planetName);
+            return response;
 
         } catch (RestClientException e) {
-            logger.error(e.getMessage(), e);
+            log.error(e.getMessage(), e);
             throw new SWAPIException(this.message.getMessage("error.message.when.consult.swapi"));
         }
-
-        return null;
     }
 
     public Integer getQuantityOfApparitionInMovies(final String planetName, final SwapiDto returnOfSwapi) {
