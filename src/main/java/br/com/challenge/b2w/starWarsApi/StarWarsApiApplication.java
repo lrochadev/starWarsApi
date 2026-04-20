2fc19ad96d17f12730dfd1fcc8bcd8964c26dd54
Leonardo Rocha <lrochadev@gmail.com>
2026-04-19 22:11:06 -0300

    feat: Enable asynchronous task execution
    Integrates Spring's `@EnableAsync` annotation, allowing methods marked with `@Async` to run on a separate thread pool. This enhances application responsiveness by offloading potentially long-running operations.

--- src/main/java/br/com/challenge/b2w/starWarsApi/StarWarsApiApplication.java
+++ src/main/java/br/com/challenge/b2w/starWarsApi/StarWarsApiApplication.java
@@ -5,12 +5,14 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cache.annotation.EnableCaching;
 import org.springframework.data.mongodb.config.EnableMongoAuditing;
 import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
+import org.springframework.scheduling.annotation.EnableAsync;
 
 /**
  * @author Leonardo Rocha
  */
 @SpringBootApplication
 @EnableCaching
+@EnableAsync
 @EnableMongoRepositories(value = "br.com.challenge.b2w.starWarsApi.repository", considerNestedRepositories = true)
 @EnableMongoAuditing
 public class StarWarsApiApplication {
